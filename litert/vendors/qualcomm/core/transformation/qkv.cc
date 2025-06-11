13b12bf68a33512ab1bfa8ffcca502101e43ae57
jiunkaiy <quic_jiunkaiy@quicinc.com>
2025-06-11 18:13:51 +0800

    init qkv pr but has some graph connection issue

--- litert/vendors/qualcomm/core/transformation/qkv.cc
+++ litert/vendors/qualcomm/core/transformation/qkv.cc
@@ -3,16 +3,27 @@
 
 #include "litert/vendors/qualcomm/core/transformation/qkv.h"
 
+#include "litert/vendors/qualcomm/core/builders/reshape_op_builder.h"
+#include "litert/vendors/qualcomm/core/builders/split_op_builder.h"
 #include "litert/vendors/qualcomm/core/utils/log.h"
-
 namespace qnn {
 namespace {
 constexpr size_t kFCIndex = 0;
 // constexpr size_t kReshape1Index = 1;
 constexpr size_t kReshape2Index = 2;
-constexpr size_t kSliceQIndex = 3;
+// constexpr size_t kSliceQIndex = 3;
 constexpr size_t kSliceKIndex = 4;
 constexpr size_t kSliceVIndex = 5;
+constexpr size_t kQRmsNormIndex = 6;
+// constexpr size_t kKRmsNormIndex = 7;
+// constexpr size_t kQSlice1Index = 8;
+// constexpr size_t kQSlice2Index = 9;
+constexpr size_t kConcatIndex = 10;
+constexpr size_t kMulCosIndex = 11;
+constexpr size_t kMulSinIndex = 12;
+constexpr size_t kAddIndex = 13;
+constexpr size_t kMHAMulIndex = 25;
+constexpr size_t kMHAOffset = 11;
 constexpr size_t kNumHeads = 4;
 constexpr size_t kNumKVHeads = 1;
 
@@ -80,11 +91,11 @@ size_t SplitQKV(std::function<bool(OpWrapper&)> validate_op_config,
                   {const_cast<TensorWrapper&>(
                       ops[start_index + kSliceVIndex].GetOutputTensor(0))});
   // Q
-  std::vector<TensorWrapper&> q_tensors;
+  std::vector<TensorWrapper*> q_tensors;
   q_tensors.reserve(kNumHeads);
   for (int i = 1; i <= kNumHeads; ++i) {
     std::vector<int8_t> q_filter(
-        filter_data.begin(),
+        filter_data.begin() + head_size * (i - 1) * filter_tensor.GetDim(1),
         filter_data.begin() + head_size * i * filter_tensor.GetDim(1));
     auto& q_filter_tensor = tensor_pool.CreateStaticTensor(
         filter_tensor.GetDataType(), filter_tensor.GetQuantParams(),
@@ -95,28 +106,104 @@ size_t SplitQKV(std::function<bool(OpWrapper&)> validate_op_config,
         fc_output, {fc_output.GetDim(0), static_cast<uint32_t>(head_size)});
     EmplaceOpWithIO(new_ops, ops[start_index + kFCIndex],
                     {std::nullopt, q_filter_tensor}, {q});
-    auto& q_after_reshape =
+    QNN_LOG_INFO("Reshape %d", ops[start_index + kReshape2Index].GetOpCode());
+    auto& q_rmsnorm_input =
         tensor_pool.CloneNativeTensorFrom(q, {1, 1, q.GetDim(0), q.GetDim(1)});
-    q_tensors.emplace_back(q_after_reshape);
+    q_tensors.emplace_back(&q_rmsnorm_input);
     EmplaceOpWithIO(new_ops, ops[start_index + kReshape2Index], {q},
-                    {q_after_reshape});
+                    {q_rmsnorm_input});
+    QNN_LOG_INFO("RMSNorm %d", ops[start_index + kQRmsNormIndex].GetOpCode());
+    auto& q_rmsnorm_output = tensor_pool.CloneNativeTensorFrom(q_rmsnorm_input);
+    EmplaceOpWithIO(new_ops, ops[start_index + kQRmsNormIndex],
+                    {q_rmsnorm_input, std::nullopt, std::nullopt},
+                    {q_rmsnorm_output});
+    const std::array<int32_t, 1> split_axis_data{3};
+    auto& split_axis = tensor_pool.CreateStaticTensor(
+        QNN_DATATYPE_INT_32, {}, {split_axis_data.size()},
+        split_axis_data.size() * sizeof(split_axis_data[0]),
+        split_axis_data.data());
+    auto split_out_dims = q_rmsnorm_input.GetDims();
+    split_out_dims[3] = split_out_dims[3] / 2;
+    const uint32_t num_split = 2;
+    auto& q_split_output1 =
+        tensor_pool.CloneNativeTensorFrom(q_rmsnorm_input, split_out_dims);
+    auto& q_split_output2 =
+        tensor_pool.CloneNativeTensorFrom(q_rmsnorm_input, split_out_dims);
+    auto split = BuildSplitOp(tensor_pool, {split_axis, q_rmsnorm_output},
+                              {q_split_output1, q_split_output2}, num_split);
+    std::move(split.begin(), split.end(), std::back_inserter(new_ops));
+    QNN_LOG_INFO("Concat %d", ops[start_index + kConcatIndex].GetOpCode());
+    auto& q_concat_output = tensor_pool.CloneNativeTensorFrom(q_rmsnorm_input);
+    EmplaceOpWithIO(new_ops, ops[start_index + kConcatIndex],
+                    {q_split_output2, q_split_output1}, {q_concat_output});
+    // Additional reshape.
+    const auto& cos_tensor = ops[start_index + kMulCosIndex].GetInputTensor(1);
+    auto& q_mul_cos_input = tensor_pool.CloneNativeTensorFrom(q_concat_output);
+    auto reshape_cos = BuildReshapeOp(
+        tensor_pool, {const_cast<::qnn::TensorWrapper&>(cos_tensor)},
+        {q_mul_cos_input});
+    std::move(reshape_cos.begin(), reshape_cos.end(),
+              std::back_inserter(new_ops));
+    QNN_LOG_INFO("MulCos %d", ops[start_index + kMulCosIndex].GetOpCode());
+    auto& q_mul_cos_output = tensor_pool.CloneNativeTensorFrom(q_concat_output);
+    EmplaceOpWithIO(new_ops, ops[start_index + kMulCosIndex],
+                    {q_rmsnorm_output, q_mul_cos_input}, {q_mul_cos_output});
+    // Additional reshape.
+    const auto& sin_tensor = ops[start_index + kMulSinIndex].GetInputTensor(1);
+    auto& q_mul_sin_input = tensor_pool.CloneNativeTensorFrom(q_concat_output);
+    auto reshape_sin = BuildReshapeOp(
+        tensor_pool, {const_cast<::qnn::TensorWrapper&>(sin_tensor)},
+        {q_mul_sin_input});
+    std::move(reshape_sin.begin(), reshape_sin.end(),
+              std::back_inserter(new_ops));
+    QNN_LOG_INFO("MulSin %d", ops[start_index + kMulSinIndex].GetOpCode());
+    auto& q_mul_sin_output = tensor_pool.CloneNativeTensorFrom(q_concat_output);
+    EmplaceOpWithIO(new_ops, ops[start_index + kMulSinIndex],
+                    {q_concat_output, q_mul_sin_input}, {q_mul_sin_output});
+    QNN_LOG_INFO("Add %d abs %d", ops[start_index + kAddIndex].GetOpCode(),
+                 start_index + kAddIndex);
+    for (int k = 0; k < 60; ++k) {
+      QNN_LOG_INFO("What %d abs %d",
+                   ops[start_index + kAddIndex + k].GetOpCode(),
+                   start_index + kAddIndex + k);
+    }
+    auto& q_add_output = tensor_pool.CloneNativeTensorFrom(q_mul_sin_output);
+    EmplaceOpWithIO(new_ops, ops[start_index + kAddIndex],
+                    {q_mul_cos_output, q_mul_sin_output}, {q_add_output});
+    // Connect add output to each SHA input.
+    QNN_LOG_INFO(
+        "MatMul %d abs %d",
+        ops[start_index + kMHAMulIndex + kMHAOffset * (i - 1)].GetOpCode(),
+        start_index + kMHAMulIndex + kMHAOffset * (i - 1));
+    EmplaceOpWithIO(new_ops,
+                    ops[start_index + kMHAMulIndex + kMHAOffset * (i - 1)],
+                    {q_add_output, std::nullopt}, {std::nullopt});
   }
 
   // Validate new graph.
+  // TODO(jiunkaiy): Disable bypassing Split int16 op validator.
   const bool is_valid =
       std::all_of(new_ops.begin(), new_ops.end(),
                   [validate_op_config](::qnn::OpWrapper& op_wrapper) -> bool {
-                    return validate_op_config(op_wrapper);
+                    return op_wrapper.IsOpCode(QnnOpCode::kSplit) ||
+                           op_wrapper.IsOpCode(QnnOpCode::kRmsNorm) ||
+                           validate_op_config(op_wrapper);
                   });
   if (is_valid) {
-    // Replace the matched pattern with a newly generated subgraph.
     size_t step_size = new_ops.size();
-    pattern_size = 6;
+    // Erase Transpose->Reshape->Split->ElementWiseMul*4.
+    for (int i = 3; i >= 0; i--) {
+      ops.erase(ops.begin() + start_index + kMHAMulIndex + kMHAOffset * i);
+    }
+    ops.erase(ops.begin() + start_index + kMHAMulIndex - 3,
+              ops.begin() + start_index + kMHAMulIndex);
+    // Replace the matched pattern with a newly generated subgraph.
     ops.insert(ops.begin() + start_index + pattern_size,
                std::make_move_iterator(new_ops.begin()),
                std::make_move_iterator(new_ops.end()));
     ops.erase(ops.begin() + start_index,
               ops.begin() + start_index + pattern_size);
+
     return step_size;
   }
   QNN_LOG_WARNING(
